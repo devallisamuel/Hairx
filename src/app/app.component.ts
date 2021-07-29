c50b356a6a52b2b0ef7d08f6a371fbd2a62247a1
Alli samuel <samueltemiloluwaalli@gmail.com>
2021-07-29 16:12:14 +0100

    added route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
 
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+public isScreenSmall:boolean;
+constructor(private breakpointObserver:BreakpointObserver) { }
 
+ngOnInit(): void {
+  this.breakpointObserver
+    // .observe([Breakpoints.XSmall]);
+  .observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+  .subscribe((state:BreakpointState) => {
+    this.isScreenSmall = state.matches;
+  });
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {  FlexLayoutModule  } from '@angular/flex-layout';
+import {  RouterModule  } from '@angular/router';
 import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SideNavComponent } from './shared/side-nav/side-nav.component';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 import { MainContentComponent } from './shared/main-content/main-content.component';
 import { MenuComponent } from './shared/menu.component';
+import { ErrorComponent } from './error.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideNavComponent,
     ToolbarComponent,
     MainContentComponent,
-    MenuComponent
+    MenuComponent,
+    ErrorComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    RouterModule.forRoot([
+      { path:'home', component: MainContentComponent },
+      { path:'', redirectTo:'/home', pathMatch:'full' },
+      { path:'**', component: ErrorComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
